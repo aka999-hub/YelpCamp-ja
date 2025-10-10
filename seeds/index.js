d33e049ece7a65d9e66ab87bff9dc23cf0079700
aka999-hub <hoge999@gmail.com>
2025-10-10 03:48:51 +0000

    fixed seeds/index.js authorID

--- seeds/index.js
+++ seeds/index.js
@@ -36,7 +36,7 @@ const seedDB = async () => {
 
         const price = Math.floor(Math.random() * 2000) + 1000   // 1000~3000円
         const camp = new Campground({
-            author: '686ed5afd19f00333f11bb1f',
+            author: '68e880cbee91dc0325b3b3b3',
             location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
             title: `${sample(descriptors)}・${sample(places)}`,
             description: '木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。東ざかいの桜沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、二十二里余にわたる長い谿谷の間に散在していた。道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。',
